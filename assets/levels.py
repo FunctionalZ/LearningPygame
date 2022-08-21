95149383d3e31d57291e69fe817cc054e762324c
FunctionalZ <Functional200@gmail.com>
2022-08-21 13:01:48 -0400

    roll back to before physics changes

--- assets/levels.py
+++ assets/levels.py
@@ -7,19 +7,19 @@ class Level(pygame.sprite.Sprite):
     def __init__(self, color, width, height):
         super().__init__()
 
-        #set characters image
+        #set characters image to level 1
         self.image = pygame.image.load("assets/levelOne.png").convert_alpha()
 
         self.rect = self.image.get_rect()
     
     def moveRight(self, pixels):
-        self.rect.x - pixels
+        self.rect.x -= pixels
         
     def moveLeft(self, pixels):
-        self.rect.x + pixels
+        self.rect.x += pixels
 
     def moveUp(self, pixels):
-        self.rect.y + pixels
+        self.rect.y += pixels
 
     def moveDown(self, pixels):
-        self.rect.y - pixels
+        self.rect.y -= pixels
